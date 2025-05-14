d0bd001cf4484033810ddb601f74e10e9f64b5e7
v_oac <62923872+voac@users.noreply.github.com>
2025-05-14 14:51:04 +0900

    Added files

---
+++ 1학년/인터넷과웹기초/실습 준비/Lab 05 ( Javascript Part 1 )/8. How to declare variables.js
@@ -0,0 +1,24 @@
+// var, let
+// Declare variables, and the stored values can be changed during runtime of the JS code.
+
+// const
+// Declare CONSTANT variable, and the stored value cannot be changed.
+
+// (nothing)
+// Without var, let, const, a variable can still be created.
+
+var score; // declare variable "score"
+var year, month, day; // declare three variables: year, month, day
+var address = "Busan"; // declare variable "address" and initialize with "Busan"
+
+// let is same as above.
+
+const PI = 3.141592;
+PI = 3.14 // This will give an error
+PI = PI + 10; // This will also give an error
+
+age = 21; // a variable is declared and initialized without var/let
+
+// We don't need to consider the data type while using JS.
+
+// JS variable can be used to store different data types DYNAMICALLY.
