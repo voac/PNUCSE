ffd6a456348b00ef29351d90cde01e9e02bcf1c8
v_oac <62923872+voac@users.noreply.github.com>
2025-05-19 15:36:46 +0900

    Added files

---
+++ 1학년/인터넷과웹기초/실습 준비/Lab 07 ( Javascript Part 2 )/let and var.js
@@ -0,0 +1,10 @@
+var x = 1;
+var x = 2; // Allowed but this is possibly a mistake.
+
+let y = 1;
+// let y = 2; // NOT allowed.
+
+const MAX = 10;
+// MAX = 20; // NOT allowed.
+
+// const MAX = 20; // NOT allowed.
