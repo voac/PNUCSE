a0208167fad34a9a161c2ac5b2b48ae9eeae7483
Hyeonbin Jang <62923872+voac@users.noreply.github.com>
2025-09-08 16:19:46 +0900

    Added files

---
+++ 1학년/2학기/기초컴퓨터프로그래밍/C_test/test01.c
@@ -0,0 +1,15 @@
+#include <stdio.h> // pre-procesor
+
+// main function
+int main(void)
+{
+    // statements (console output)
+    printf("Hello,\nWorld !\n");
+
+    printf("%d\n", 100); // integer
+    printf("%f\n", 3.28); // float
+    printf("%c\n", 'A'); // character
+
+    // return with code 0
+    return 0;
+}
